6ae4c26709fd665a13052346758409d34c5f9af7
vlad <gorb.vd@gmail.com>
2022-01-24 17:34:58 +0700

    fix

--- adapters/card/CardDeleteAdapter.ts
+++ adapters/card/CardDeleteAdapter.ts
@@ -1,12 +1,33 @@
 import { ProductId } from "../../domain/entities/product/productEntities";
 import { CardDeletePort } from "../../domain/ports/out/card/CardDeletePort";
 import { right, left } from "@sweet-monads/either";
+import { SuccessResponseSchema } from "../../schema/successSchema";
+import { ErrorEntities } from "../../domain/entities/errorEntities";
+import { SuccessEntities } from "../../domain/entities/SuccessEntities";
 
 export class CardDeleteAdapter implements CardDeletePort {
-  delete(ids: ProductId[]) {
+  api(ids: ProductId[]) {
+    let isDelete = false;
     process.api.cards = process.api.cards.filter((product) => {
-      return !ids.includes(product.id);
+      const validId = !ids.includes(product.id);
+      if (validId) {
+        isDelete = true;
+      }
+      return validId;
     });
-    return right({ success: true });
+    return {
+      data: { success: isDelete },
+      code: 200,
+    };
+  }
+
+  delete(ids: ProductId[]) {
+    const response = this.api(ids);
+    const valid = SuccessResponseSchema.safeParse(response.data);
+    if (valid.success) {
+      return right(new SuccessEntities(true));
+    } else {
+      return left(new ErrorEntities("id valid"));
+    }
   }
 }
--- domain/ports/in/card/CardDeleteUseCase.ts
+++ domain/ports/in/card/CardDeleteUseCase.ts
@@ -1,11 +1,7 @@
 import { ProductId } from "../../../entities/product/productEntities";
 import { Either } from "@sweet-monads/either";
 import { ErrorEntities } from "../../../entities/errorEntities";
+import { SuccessEntities } from "../../../entities/SuccessEntities";
 export interface CardDeleteUseCase {
-  delete(ids: ProductId[]): Either<
-    ErrorEntities,
-    {
-      success: boolean;
-    }
-  >;
+  delete(ids: ProductId[]): Either<ErrorEntities, SuccessEntities>;
 }
